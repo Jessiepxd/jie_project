3225adb60ce5a7c6f52ac2b79d3f887499ea5e9d
Jie Chen <chenjiesunny@gmail.com>
2023-02-14 07:52:56 -0500

    basic starter code

---
+++ src/jieproject/Card.java
@@ -0,0 +1,25 @@
+/**
+ * SYST 17796 Project Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as an author and the date!
+ */
+
+package jieproject;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Jie Chen
+ */
+public abstract class Card {
+    //default modifier for child classes
+
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    @Override
+    public abstract String toString();
+
+}
---
+++ src/jieproject/JieProject.java
@@ -0,0 +1,19 @@
+
+
+package jieproject;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Jie Chen
+ */
+public class JieProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
